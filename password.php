be9f5242dc9645e52cd0042991f8ee3bc5607dcc
Juho 'darKsy' Nikula <juho.nikula@hotmail.com>
2015-11-03 10:30:32 +0100

    Added comments to password.php

--- password.php
+++ password.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ *  Note: password_hash and password_verify were added in PHP 5.5.0
+ *  if using an older php version please see: 
+ *  https://github.com/ircmaxell/password_compat instead.
+ *
+ *  PASSWORD_DEFAULT is currently the same as PASSWORD_BCRYPT this might change
+ *  when a better hashing algorithm has proven it self.
+ */
+
 //Set work factor for bcrypt
 $options = [
     "cost" => 14,
