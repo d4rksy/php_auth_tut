4eb7e5241f7b38594bfa31d5aa0b209f88793aac
Juho <juho.nikula@hotmail.com>
2015-11-04 10:54:22 +0100

    Added docblock comments to database.php

--- database.php
+++ database.php
@@ -4,7 +4,12 @@
 $conn = new PDO('mysql:host=localhost;dbname=test', 'root', '');
 
 
-// Grab data relating to username
+/**
+ * Get User data, hashed password, username, email etc.
+ * @param  string $username 
+ * @param  object PDO $conn
+ * @return array 
+ */
 function getUser($username,$conn) {
     $query = $conn->prepare("SELECT * FROM profile WHERE username = :username LIMIT 1");
     $query->bindParam(":username", $username);
@@ -13,6 +18,35 @@ function getUser($username,$conn) {
     return $profile;
 }
 
+/**
+ * Get specific users comments with additional criteria if chosen.
+ * @param  array (contains user_id and option array() for detailed searches)
+ * @param  $conn PDO object
+ * @return array (contains results)
+ */
+function getUserComments($data = array(),$conn) {
+    if(isset($data["options"]["range"])) {
+        $query = $conn->prepare("SELECT * FROM guestbook WHERE user_id = :user_id AND timestamp > :range");
+        $query->bindParam(":user_id", $data["user_id"]);
+        $query->bindParam(":range", $data["options"]["range"]);
+        $query->execute();
+        $userComments = $query->fetchAll();
+        return $userComments;
+    }
+    else {
+        $query = $conn->prepare("SELECT * FROM guestbook WHERE user_id = :user_id LIMIT 20");
+        $query->bindParam(":user_id", $data["user_id"]);
+        $query->execute();
+        $userComments = $query->fetchAll();
+        return $userComments;
+    }
+}
+
+/**
+ * Insert user data from registration or admin creation
+ * @param  $data array containing user data
+ * @param  $conn PDO object
+ */
 function insertUser($data,$conn) {
     $query = $conn->prepare("INSERT INTO profile (username, password, email) VALUES (:username, :password, :email)");
     $query->bindParam(":username", $data["username"]);
@@ -21,6 +55,10 @@ function insertUser($data,$conn) {
     $query->execute();
 }
 
+/**
+ * Creates the user table if not present.
+ * @param  object
+ */
 function createUserTable($conn) {
     $query = $conn->prepare("CREATE TABLE `profile` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,6 +71,10 @@ function createUserTable($conn) {
     $query->execute();
 }
 
+/**
+ * Creates the Guestbook table if not present
+ * @param  object
+ */
 function createGuestbookTable($conn) {
     $query = $conn->prepare("CREATE TABLE `guestbook` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -44,6 +86,11 @@ function createGuestbookTable($conn) {
     $query->execute();
 }
 
+/**
+ * Inserts comment data
+ * @param  object
+ * @param  $data array containing comment data
+ */
 function insertComment($conn, $data) {
     $query = $conn->prepare("INSERT INTO guestbook (comment, timestamp, user_id) VALUES (:comment, :timestamp, :user_id)");
     $query->bindParam(":comment", $data["comment"]);
@@ -52,6 +99,12 @@ function insertComment($conn, $data) {
     $query->execute();
 }
 
+/**
+ * Checks for the existance of a table by attempting to select the first column in the table. 
+ * @param  $conn object
+ * @param  $table string
+ * @return $result boolean
+ */
 function tableExists($conn, $table) {
 
     // Try a select statement against the table
@@ -67,11 +120,15 @@ function tableExists($conn, $table) {
     return $result !== FALSE;
 }
 
+/**
+ * @param  $conn object
+ * @return $guestbook array
+ */
 function fetchGuestbook($conn) {
     $query = $conn->prepare("SELECT profile.username, guestbook.comment, guestbook.timestamp FROM guestbook JOIN profile on profile.id=guestbook.user_id ORDER BY guestbook.timestamp DESC");
     $query->execute();
-    $profile = $query->fetchAll();
-    return $profile;
+    $guestbook = $query->fetchAll();
+    return $guestbook;
 }
 
 ?>
